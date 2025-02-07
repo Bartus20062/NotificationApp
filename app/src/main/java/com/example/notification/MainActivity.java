c1689dfc4c046edf1692a930eb1d7049d6c1874f
9578845u <bartoszposila@gmail.com>
2025-02-07 11:06:38 +0100

    Notification

---
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.notification;
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String CHANNEL_ID = "my_channel_id";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        createNotificationChannel();
+        findViewById(R.id.button1).setOnClickListener(v->sendNotificationLong());
+        createNotificationChannel();
+        findViewById(R.id.button2).setOnClickListener(v->sendNotification());
+        createNotificationChannel();
+        findViewById(R.id.button3).setOnClickListener(v->sendNotificationBigPicture());
+    }
+
+    private void createNotificationChannel(){
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+            CharSequence name = "Kanał Powiadomień";
+            String description = "Opis kanału powiadomień";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+    private void sendNotification(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.TIRAMISU){
+            if(checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS)
+                != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+                return;
+            }
+        }
+
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.aniel)
+                        .setContentTitle("Nowe Powiadomienie dla 4TPE")
+                        .setContentText("To jest twoje powiadomienie")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setAutoCancel(true);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(2, builder.build());
+    }
+    private void sendNotificationLong(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.TIRAMISU){
+            if(checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS)
+                    != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+                return;
+            }
+        }
+
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.aniel)
+                        .setContentTitle("Nowe Powiadomienie dla 4TPE")
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText("Gdy ściskasz mu ręką on w szafie chowa Ci trupa\n" +
+                                "Kapuś kapuś inaczej 60-tka\n" +
+                                "Bez skrupułów sprzeda najlepszego ziomka\n" +
+                                "Kapuś kapuś jebana ściera\n" +
+                                "Grał gangstera dziś ma status frajera\n" +
+                                "Kapuś kapuś inaczej 60-tka\n" +
+                                "Bez skrupułów sprzeda najlepszego ziomka\n" +
+                                "Kapuś kapuś jebana ściera\n" +
+                                "Grał gangstera dziś ma status frajera\n" +
+                                "Ktoś podał Ci rękę Ty później ją zakułeś\n" +
+                                "\n" ))
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setAutoCancel(true);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(1, builder.build());
+    }
+    private void sendNotificationBigPicture(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.TIRAMISU){
+            if(checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS)
+                    != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+                return;
+            }
+        }
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.image);
+
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.image)
+                        .setContentTitle("Nowe Powiadomienie dla 4TPE")
+                        .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(bitmap)
+                                .setBigContentTitle("Powiadomienie z duzym obrazkiem"))
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setAutoCancel(true);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(3, builder.build());
+    }
+}
